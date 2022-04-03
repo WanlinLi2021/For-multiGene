98443ed26d93307eefac743be77838d837da2b0e
Wanlin Li <53668767+WanlinLi2021@users.noreply.github.com>
2022-04-03 19:59:20 -0400

    Add files via upload

---
+++ apps/addGene.py
@@ -0,0 +1,110 @@
+import dash
+from dash import Dash, html, dcc
+#from dash import dcc
+#from dash import html
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output, State
+import plotly.express as px
+import pandas as pd
+from app import app
+import os
+import base64
+from Bio import SeqIO
+from requests import get
+from apps import geneParam, logPage
+
+#----------------------------------------
+def getIpAdress():
+    ip = get("https://api.ipify.org").text
+    return ip
+theIp = getIpAdress()
+    
+ #create user's file
+user_input = 'user/'  + theIp +'/input' 
+user_output = 'user/'  + theIp +'/output' 
+if not os.path.exists(user_input):
+    os.makedirs(user_input)
+if not os.path.exists(user_output):
+    os.makedirs(user_output)
+csv_file = user_input + '/donnees.csv'
+seq_file = user_input + '/upload_gene.fasta'
+if os.path.exists(csv_file):
+  os.remove(csv_file)
+
+if os.path.exists(seq_file):
+  os.remove(seq_file)
+
+#-------------------------------------------
+# create a csv table for gene parameters
+genes_csv = 'user/' + theIp + '/input/parameters.csv'
+if not os.path.exists(genes_csv):
+    with open(genes_csv, 'w') as f:
+        f.write("Gene,Bootstrap value threshold,Robinson and Foulds distance threshold,Sliding Window Size,Step Size\n")
+
+
+#-------------------------------------------
+layout = dbc.Container([
+    html.H1('Phylogenetic Tree', style={"textAlign": "center"}),  #title
+    
+    #add gene button
+    dbc.Row([
+        dbc.Col([
+            html.Br(),
+            html.Button(id="add-button", children="Add Gene"),
+            html.Br(),
+            html.Hr(),
+        ], #width={'size':3, 'offset':1, 'order':1},
+           xs=12, sm=12, md=12, lg=10, xl=10
+        ),
+    ], justify='around'),
+
+    # add more genes
+    dbc.Row([
+            dbc.Col([
+                html.Div(id='addGene-container'),
+                #html.Hr(),
+                ],xs=12, sm=12, md=12, lg=10, xl=10),
+
+            ], justify='around'),
+    #add gene button
+    dbc.Row([
+        dbc.Col([
+            html.Br(),
+            html.Button(id="addMore-button", children="Add Another Gene"),
+            html.Br(),
+            html.Hr(),
+        ], #width={'size':3, 'offset':1, 'order':1},
+           xs=12, sm=12, md=12, lg=10, xl=10
+        ),
+    ], justify='around'),
+    # log
+    dbc.Row([
+            dbc.Col([
+                html.Div(logPage.layout),
+                #html.Hr(),
+                ],xs=12, sm=12, md=12, lg=10, xl=10),
+
+            ], justify='around'),
+    
+], fluid=True)
+
+#-----------------------------------
+@app.callback(
+    Output('addGene-container', 'children'),
+    Input("add-button", "n_clicks"),
+    Input("addMore-button", "n_clicks"),
+    )
+
+def update_output(add, add_more):
+    gene_param = dbc.Container([
+                html.Div(geneParam.layout),    
+                        ], fluid=True)
+
+    if add and add_more is None:
+        return dash.no_update
+    else:
+        return gene_param
+        
+            
+
+#-------------------------------------------
